44b9d87ba3146e0f4b5b3625733b0e055e916455
Macarena Ruiz <contactomruiz@yahoo.com>
2022-05-05 12:52:47 -0300

    ataque

--- My project/Assets/Scripts/BalaControl.cs
+++ My project/Assets/Scripts/BalaControl.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class BalaControl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 5f;
+    public float deactivateTimer = 3f;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 temp = transform.position;
+        temp.x += speed * Time.deltaTime;
+        transform.position = temp;
     }
 }
--- My project/Assets/Scripts/MainShipControl.cs
+++ My project/Assets/Scripts/MainShipControl.cs
@@ -6,15 +6,23 @@ public class MainShipControl : MonoBehaviour
 {
     public float speed = 5f;
     public float minY, maxY;
+
+    [SerializeField] private GameObject MainBala;
+    [SerializeField] private Transform AtackPoint;
+
+    public float atackTimer = 0.35f;
+    private float currentAtackTimer;
+    private bool canAtack;
     void Start()
     {
-        
+        currentAtackTimer = atackTimer;
     }
 
     // Update is called once per frame
     void Update()
     {
         MoveShip();
+        Atacar();
     }
 
     void MoveShip()
@@ -37,6 +45,26 @@ public class MainShipControl : MonoBehaviour
                 temp.y = minY;
 
             transform.position = temp;
+        }
+    }
+
+    void Atacar()
+    {
+        atackTimer += Time.deltaTime;
+        if (atackTimer > currentAtackTimer)
+        {
+            canAtack = true;
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (canAtack) 
+            {
+                canAtack = false;
+                atackTimer = 0f;
+
+                Instantiate(MainBala, AtackPoint.position, AtackPoint.transform.rotation);
+            }
         }
     }
 }
